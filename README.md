1a404674e31456f5a345ec95ea587ce2d3fa88f1
Pulkit Gera <pulkit.gera@research.iiit.ac.in>
2020-11-11 12:49:20 +0530

    added code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Topic-Modelling
+![](tan1_topic_graph.png)
 Topic Modelling and Trends across keywords during MeToo movement in India
+We do analysis of the tweets trending with #MeToo movement and try to understand what sort of topics were being discussed.
+
+## Installation
+With the environment file given, simply do
+`conda env create -f environment.yml`
+
+## Usage
+### Tweets URL
+Firstly we use snscrape to get the tweets we want. The basic usage is as follows - 
+`snscrape  twitter-search "#metoo aib  since:2018-10-26 until:2018-11-26 lang:en " > tweets_url.csv`
+You can use this to scrape instagram as well. More about it [here](https://github.com/JustAnotherArchivist/snscrape)  
+
+### Scrape Tweets
+We use tweepy library to scrape the tweets. This will return the tweets and other info. 
+`python extract.py tweets_url.csv tweets.csv`
+
+### Analysis
+We do [Topic Modelling using LDA](https://towardsdatascience.com/topic-modeling-and-latent-dirichlet-allocation-in-python-9bf156893c24) to understand the topics in a more coherent fashion. This is an unsupervised technique to understand the context of the tweets and accordingly build on it. We use this method to understand the public sentiment and reactions during the phase of #Metoo
---
+++ extract.py
@@ -0,0 +1,42 @@
+import pandas as pd 
+import tweepy
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+def fetch_tw(ids,output_file):
+    list_of_tw_status = api.statuses_lookup(ids, tweet_mode= "extended")
+    empty_data = pd.DataFrame()
+    for status in list_of_tw_status:
+            tweet_elem = {"tweet_id": status.id,
+                     "screen_name": status.user.screen_name,
+                     "tweet":status.full_text,
+                     "date":status.created_at,
+                     "likes":status._json['favorite_count'],
+                     "retweets":status._json['retweet_count'],
+                     "location":status.user.location,
+                     "coordinates":status.coordinates,
+                     }
+            empty_data = empty_data.append(tweet_elem, ignore_index = True)
+    empty_data.to_csv(output_file, mode="a")
+
+consumer_key = "DgenSt0OxyaCcMuuJyMCdkJGd"
+consumer_secret = "o1lDfV4AG0tWufIHXGtjY6bNSuAdrSL9hJs4onZRDvEKNMS9t8"
+access_token = "1111593376804028416-yhGKowC8Xpd8HNUDNeS7fjjKsZyreT"
+access_token_secret = "bfJX8z1PrIcsui8W5CuUDBB48ii3pcA9TRKSMw4sadCdn"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+tweet_url = pd.read_csv(input_file, index_col= None, header = None, names = ["links"])
+af = lambda x: x["links"].split("/")[-1]
+tweet_url['id'] = tweet_url.apply(af, axis=1)
+ids = tweet_url['id'].tolist()
+total_count = len(ids)
+chunks = (total_count - 1) // 50 + 1
+print(tweet_url.head())
+for i in range(chunks):
+        batch = ids[i*50:(i+1)*50]
+        result = fetch_tw(batch,output_file)
